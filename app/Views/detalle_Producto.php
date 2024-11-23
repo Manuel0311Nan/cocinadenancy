2ae83ebfa6305513ad17eae5ea5c9e7ddab53b0d
Manuel0311Nan <manuzz3112@gmail.com>
2024-11-23 09:27:03 +0100

    fix: gallery options responsive

--- app/Views/detalle_Producto.php
+++ app/Views/detalle_Producto.php
@@ -1,5 +1,5 @@
-        <div class="flex flex-col lg:flex-row h-screen">
-            <div class="w-full lg:w-1/2 bg-gray-200 h-1/2 lg:h-full">
+        <div class="flex flex-col xl:flex-row h-screen">
+            <div class="w-full xl:w-1/2 bg-gray-200 h-1/2 xl:h-full">
                 <?php if ($producto['imagen'] == '') { ?>
                     <img class="w-full h-full object-cover" src="https://res.cloudinary.com/de3ujeyub/image/upload/v1731863550/mauro-lima-WjybIjpcf0s-unsplash_jo6byx.jpg" alt="productPorDefecto">
                 <?php } else { ?>
@@ -7,9 +7,9 @@
                 <?php } ?>
 
             </div>
-            <div class="w-full lg:w-1/2 bg-white p-8 overflow-y-auto">
+            <div class="w-full xl:w-1/2 bg-white p-8 overflow-y-auto">
                 <h1 class="text-4xl font-bold text-gray-900 mb-8">Opciones de compra</h1>
-                <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+                <div class="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-2 gap-8">
                     <?php foreach ($opciones_compra as $opcion): ?>
                         <div class="bg-white shadow-lg rounded-lg overflow-hidden p-6">
                             <?php if (esc($opcion['cantidad']) != null): ?>
