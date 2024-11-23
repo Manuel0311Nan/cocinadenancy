85b488a15c3ad035b499dc850115dd37083937c9
Manuel0311Nan <manuzz3112@gmail.com>
2024-11-23 09:19:46 +0100

    fix: format archives

--- app/Views/contacto.php
+++ app/Views/contacto.php
@@ -14,11 +14,17 @@
             <label for="email" class="block text-gray-700 text-sm font-bold mb-2">Correo electrónico:</label>
             <input type="email" name="email" id="email" required class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
         </div>
+        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+            <div>
+                <button type="button" id="verCarrito" class="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+                    Ver Pedido
+                </button>
+            </div>
+            <div class="flex">
+                <p>Teléfono para hacer pedidos: <span class="text-red-700 font-bold">600000000</span> </p>
+            </div>
 
-        <button type="button" id="verCarrito" class="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
-            Ver Pedido
-        </button>
-
+        </div>
         <div class="mt-8">
             <button type="submit" class="bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
                 Enviar Pedido
@@ -27,7 +33,7 @@
     </form>
 </div>
 
-<div id="carritoModal" class="fixed z-10 inset-0 overflow-y-auto hidden" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+<div id="carritoModal" class="fixed z-1 inset-0 overflow-y-auto hidden" aria-labelledby="modal-title" role="dialog" aria-modal="true">
     <div class="flex items-center justify-center min-h-screen">
         <div class="bg-white rounded-lg shadow-lg p-8 w-full max-w-3xl">
             <h2 class="text-2xl font-bold mb-4">Pedido Detallado</h2>
@@ -67,3 +73,49 @@
         </div>
     </div>
 </div>
+
+<script>
+    document.addEventListener("DOMContentLoaded", function() {
+        const verCarritoBtn = document.getElementById('verCarrito');
+        const carritoModal = document.getElementById('carritoModal');
+        const cerrarModalBtn = document.getElementById('cerrarModal');
+
+        // Abrir el modal
+        verCarritoBtn.addEventListener('click', function() {
+            carritoModal.classList.remove('hidden');
+        });
+
+        // Cerrar el modal
+        cerrarModalBtn.addEventListener('click', function() {
+            carritoModal.classList.add('hidden');
+        });
+
+        // Eliminar productos del carrito
+        const eliminarBtns = document.querySelectorAll('.eliminarProducto');
+        eliminarBtns.forEach(button => {
+            button.addEventListener('click', function() {
+                const index = button.getAttribute('data-index');
+                
+                // Realizar una solicitud para eliminar el producto del carrito
+                fetch('<?= base_url('carrito/eliminar') ?>', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ index: index })
+                })
+                .then(response => response.json())
+                .then(data => {
+                    if (data.success) {
+                        location.reload(); // Recargar la página para ver los cambios
+                    } else {
+                        alert('Error al eliminar el producto del carrito.');
+                    }
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                });
+            });
+        });
+    });
+</script>
--- app/Views/detalle_Producto.php
+++ app/Views/detalle_Producto.php
@@ -1,73 +1,78 @@
         <div class="flex flex-col lg:flex-row h-screen">
             <div class="w-full lg:w-1/2 bg-gray-200 h-1/2 lg:h-full">
-            <?php if($producto['imagen'] == ''){?>
-                <img class="w-full h-full object-cover" src="https://res.cloudinary.com/de3ujeyub/image/upload/v1731863550/mauro-lima-WjybIjpcf0s-unsplash_jo6byx.jpg" alt="productPorDefecto">
-                    <?php }else{?>
-                        <img class="w-full h-full object-cover" src="<?= ('https://res.cloudinary.com/de3ujeyub/image/upload/v1731846686/' .$producto['imagen']) ?>.jpg" alt="<?= esc($producto['nombre']) ?>">
-                    <?php } ?>
-            
+                <?php if ($producto['imagen'] == '') { ?>
+                    <img class="w-full h-full object-cover" src="https://res.cloudinary.com/de3ujeyub/image/upload/v1731863550/mauro-lima-WjybIjpcf0s-unsplash_jo6byx.jpg" alt="productPorDefecto">
+                <?php } else { ?>
+                    <img class="w-full h-full object-cover" src="<?= ('https://res.cloudinary.com/de3ujeyub/image/upload/v1731846686/' . $producto['imagen']) ?>.jpg" alt="<?= esc($producto['nombre']) ?>">
+                <?php } ?>
+
             </div>
             <div class="w-full lg:w-1/2 bg-white p-8 overflow-y-auto">
-    <h1 class="text-4xl font-bold text-gray-900 mb-8">Opciones de compra</h1>
-    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-        <?php foreach ($opciones_compra as $opcion): ?>
-            <div class="bg-white shadow-lg rounded-lg overflow-hidden p-6">
-                <?php if (esc($opcion['cantidad']) != null): ?>
-                    <h3 class="text-xl font-semibold mb-2">Opción <?= esc($opcion['cantidad']) ?> unidades</h3>
-                    <p class="text-gray-700 mb-4">Cantidad: <?= esc($opcion['cantidad']) ?> unidades</p>
-                    <p class="text-xl text-red-600 font-bold mb-4">Precio: <?= esc($opcion['precio']) ?> €</p>
-                    <button class="w-full bg-red-600 hover:bg-red-700 text-white font-semibold py-2 px-4 rounded-lg agregar-carrito"
-                            data-id="<?= esc($opcion['id']) ?>" data-cantidad="<?= esc($opcion['cantidad']) ?>">
-                        Añadir al carrito
-                    </button>
-                <?php else: ?>
-                    <div>
-                        <h3 class="text-xl font-semibold mb-2">Tamaño <?= esc($opcion['tamaño']) ?></h3>
-                        <p class="text-xl text-red-600 font-bold mb-4">Precio: <?= esc($opcion['precio']) ?> €</p>
-                        <button class="w-full bg-red-600 hover:bg-red-700 text-white font-semibold py-2 px-4 rounded-lg agregar-carrito"
-                                data-id="<?= esc($opcion['id']) ?>" data-cantidad="1">
-                            Añadir al carrito
-                        </button>
-                    </div>
-                <?php endif; ?>
+                <h1 class="text-4xl font-bold text-gray-900 mb-8">Opciones de compra</h1>
+                <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+                    <?php foreach ($opciones_compra as $opcion): ?>
+                        <div class="bg-white shadow-lg rounded-lg overflow-hidden p-6">
+                            <?php if (esc($opcion['cantidad']) != null): ?>
+                                <h3 class="text-xl font-semibold mb-2">Opción <?= esc($opcion['cantidad']) ?> unidades</h3>
+                                <p class="text-gray-700 mb-4">Cantidad: <?= esc($opcion['cantidad']) ?> unidades</p>
+                                <p class="text-xl text-red-600 font-bold mb-4">Precio: <?= esc($opcion['precio']) ?> €</p>
+                                <button class="w-full bg-red-600 hover:bg-red-700 text-white font-semibold py-2 px-4 rounded-lg agregar-carrito"
+                                    data-id="<?= esc($opcion['id']) ?>" data-cantidad="<?= esc($opcion['cantidad']) ?>">
+                                    Añadir al carrito
+                                </button>
+                            <?php else: ?>
+                                <div>
+                                    <h3 class="text-xl font-semibold mb-2">Tamaño <?= esc($opcion['tamaño']) ?></h3>
+                                    <p class="text-xl text-red-600 font-bold mb-4">Precio: <?= esc($opcion['precio']) ?> €</p>
+                                    <button class="w-full bg-red-600 hover:bg-red-700 text-white font-semibold py-2 px-4 rounded-lg agregar-carrito"
+                                        data-id="<?= esc($opcion['id']) ?>" data-cantidad="1">
+                                        Añadir al carrito
+                                    </button>
+                                </div>
+                            <?php endif; ?>
+                        </div>
+                    <?php endforeach; ?>
+                </div>
             </div>
-        <?php endforeach; ?>
-    </div>
-</div>
-    </div>
-    <script>
-    document.addEventListener("DOMContentLoaded", function() {
-        const botonesAgregar = document.querySelectorAll('.agregar-carrito');
+        </div>
+        <script>
+            document.addEventListener("DOMContentLoaded", function() {
+                const botonesAgregar = document.querySelectorAll('.agregar-carrito');
 
-        botonesAgregar.forEach(button => {
-            button.addEventListener('click', function() {
-                const idProducto = this.getAttribute('data-id');
-                const cantidad = this.getAttribute('data-cantidad');
-                console.log('Datos enviados:', { idProducto, cantidad });
-                fetch('<?= base_url('carrito/agregar') ?>', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({ idProducto: idProducto, cantidad: cantidad })
-                    
-                })
-                .then(response => response.json())
-                .then(data => {
-                    if (data.success) {
-                        alert('Producto añadido al carrito');
-                    } else {
-                        console.error('Error:', data.error);
-                        alert('Error al añadir el producto al carrito');
-                    }
-                })
-                .catch(error => {
-                    console.error('Error:', error);
-                    alert('Error al añadir el producto al carrito');
+                botonesAgregar.forEach(button => {
+                    button.addEventListener('click', function() {
+                        const idProducto = this.getAttribute('data-id');
+                        const cantidad = this.getAttribute('data-cantidad');
+                        console.log('Datos enviados:', {
+                            idProducto,
+                            cantidad
+                        });
+
+                        fetch('<?= base_url('/carrito/agregar') ?>', {
+                                method: 'POST',
+                                headers: {
+                                    'Content-Type': 'application/json'
+                                },
+                                body: JSON.stringify({
+                                    idProducto: idProducto,
+                                    cantidad: cantidad
+                                })
+
+                            })
+                            .then(response => response.json())
+                            .then(data => {
+                                if (data.success) {
+                                    alert('Producto añadido al carrito');
+                                } else {
+                                    console.error('Error:', data.error);
+                                    alert('Error al añadir el producto al carrito');
+                                }
+                            })
+                            .catch(error => {
+                                console.error('Error:', error);
+                                alert('Error al añadir el producto al carrito desde abajo');
+                            });
+                    });
                 });
             });
-        });
-    });
-</script>
-
-
+        </script>
