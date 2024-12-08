7487923d17c857979dfba20a7aa6ac92d6c2782d
Manuel0311Nan <manuzz3112@gmail.com>
2024-12-08 14:38:32 +0100

    fix: routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,12 +5,13 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
+$routes->setAutoRoute(true);
 $routes->get('/', 'Home::index');
 $routes->get('/contacto', 'Home::contacto');
 $routes->post('/envioForm', 'Contacto::send');
 $routes->get('/productos', 'Home::productos');
 $routes->get('/nosotros', 'Home::nosotros');
 $routes->get('/producto/(:segment)', 'ProductosController::obtenerDetallesProducto/$1');
-$routes->post('carrito/agregar', 'CarritoController::agregarAlCarrito');
-$routes->get('/carrito', 'CarritoController::verCarrito');
+$routes->post('/carrito/agregar', 'CarritoController::agregarAlCarrito');
+$routes->get('/contacto', 'CarritoController::verCarrito');
 $routes->post('carrito/eliminar', 'CarritoController::eliminarDelCarrito');
